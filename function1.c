9ba2b049cecc51b142806a90463e19a5d7146164
Mashia-Abilba <mashia2512@gmail.com>
2023-03-28 16:47:50 -0700

    function1.c

--- function1.c
+++ function1.c
@@ -15,6 +15,7 @@ int print_unsigned(va_list types, char buffer[],
 {
 	int i = BUFF_SIZE - 2;
 	unsigned long int num = va_arg(types, unsigned long int);
+
 	num = convert_size_unsgnd(num, size);
 
 	if (num == 0)
@@ -46,7 +47,9 @@ int print_octal(va_list types, char buffer[],
 	unsigned long int num = va_arg(types, unsigned long int);
 
 	unsigned long int init_num = num;
+
 	UNUSED(width);
+
 	num = convert_size_unsgnd(num, size);
 	if (num == 0)
 		buffer[i--] = '0';
@@ -72,9 +75,11 @@ int print_octal(va_list types, char buffer[],
  * @size: Size specifier
  * Return: Number of chars printed
  */
-int print_hexadecimal(va_list types, char buffer[],int flags, int width, int precision, int size)
+int print_hexadecimal(va_list types, char buffer[],
+		int flags, int width, int precision, int size)
 
-	return (print_hexa(types, "0123456789abcdef", buffer, flags, 'x', width, precision, size));
+	return (print_hexa(types, "0123456789abcdef", buffer,
+	flags, 'x', width, precision, size));
 }
 /************* PRINT UNSIGNED NUMBER IN UPPER HEXADECIMAL **************/
 /**
@@ -112,8 +117,10 @@ int print_hexa(va_list types, char map_to[], char buffer[],
 {
 	int i = BUFF_SIZE - 2;
 	unsigned long int num = va_arg(types, unsigned long int);
+
 	unsigned long int init_num = num;
 	UNUSED(width);
+
 	num = convert_size_unsgnd(num, size);
 	if (num == 0)
 		buffer[i--] = '0';
